895d2afd4de1e2c98e91421037369aec00d010c8
wendel <alkimo@protonmail.com>
2021-02-23 00:19:37 -0300

    IntroPage With Three Inputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hexagon/hexagon.dart';
 
 void main() {
@@ -20,30 +21,135 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  final TextEditingController _untimedDebt = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    _untimedDebt.text = "";
+    _untimedDebt.addListener(_inputValue);
+
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Financial",
-            style: TextStyle(color: Colors.white, fontSize: 90, fontFamily: 'Ubuntu'),
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [Color(0xFF0e406f), Color(0xFF05173e)])),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 55.0),
+                child: Text(
+                  "Financial",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 80,
+                    fontFamily: 'Ubuntu',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30.0),
+                child: Text(
+                  "Control",
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 50, fontFamily: 'Ubuntu'),
+                ),
+              ),
+              Text(
+                "Whats your montly income?",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'Ubuntu',
+                  color: Color(0xFF0e406f),
+                ),
+              ),
+              TextField(
+                style: TextStyle(fontSize: 55, color: Colors.white),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.white, fontSize: 55),
+                  hintText: '0.0',
+                ),
+              ),
+              Text(
+                "Your total nontime limit debt?",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'Ubuntu',
+                  color: Color(0xFF0e406f),
+                ),
+              ),
+              TextField(
+                style: TextStyle(fontSize: 55, color: Colors.white),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '0.0',
+                  hintStyle: TextStyle(color: Colors.white, fontSize: 55),
+                ),
+              ),
+              Text(
+                "Amount of deadlined debts?",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'Ubuntu',
+                  color: Color(0xFF0e406f),
+                ),
+              ),
+              TextField(
+                onChanged: _inputValue(),
+                controller: _untimedDebt,
+                style: TextStyle(fontSize: 55, color: Colors.white),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '0',
+                  hintStyle: TextStyle(color: Colors.white, fontSize: 55),
+                ),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: HexagonWidget.pointy(
+                      cornerRadius: 15,
+                      width: 100,
+                      color: Colors.white,
+                      elevation: 5,
+                      child: Text(
+                        '\$',
+                        style: TextStyle(fontSize: 55),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          Text(
-            "Control",
-            style: TextStyle(color: Colors.white, fontSize: 65, fontFamily: 'Ubuntu'),
-          ),
-          SizedBox(
-            height: 65,
-          ),
-
-
-        ],
+        ),
       ),
     );
+  }
+
+  _inputValue() {
+    if (_untimedDebt.text != "") {
+      print("Debt Value: ${_untimedDebt.text}");
+    }
   }
 }
